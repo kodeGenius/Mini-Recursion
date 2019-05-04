8e87ca39ce4d9182d5cc8dc99f70669116844e9a
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2019-05-04 21:42:58 +0100

    Optional check completed

--- mini.js
+++ mini.js
@@ -2,7 +2,7 @@
 const mini = (a, b) =>{
 	if(a > b){
 		return `${a} is greater than ${b}`}else{ 
-		
+		return `${b} is greater than ${a}`}
 	//return Math.min(a, b);
 };
 console.log(mini (13, 67))
